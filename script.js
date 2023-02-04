e60c4d1157d7fec98d31ea10cd9e1b3eedc08e95
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-02-04 23:05:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let temp = document.getElementById("temp");
+
+const tempLoad = () => {
+  temp.innerHTML = "&#xf2cb";
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+    temp.style.color = "#f8b627";
+  }, 1000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+  }, 2000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+  }, 3000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+    temp.style.color = "#d63031";
+  }, 4000);
+};
+
+tempLoad();
+
+setInterval(tempLoad, 5000);
